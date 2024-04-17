7c8e116eda0675268338443681355ce0ecc18ce4
Jacob Castanon <jacastanon01@gmail.com>
2024-04-17 10:19:41 -0500

    refactored code, kept classes related to screen in one file. Drew test points to confirm canvas is displaying lines properly

--- src/Window.py
+++ src/Window.py
@@ -1,25 +1,49 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1: Point, point2: Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
+        x1 = self.point1.x
+        y1 = self.point1.y
+        x2 = self.point2.x
+        y2 = self.point2.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
+
+
 class Window:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, bg="white")
-        self.canvas.pack()
+        self._canvas = Canvas(
+            self.__root, bg="white", height=self.height, width=self.width
+        )
+        self._canvas.pack(fill=BOTH, expand=True)
         self._is_window_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
+    def draw_line(self, line: Line, fill_color: str) -> None:
+        line.draw(self._canvas, fill_color)
+
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self._is_window_running = True
         while self._is_window_running:
             self.redraw()
 
-    def close(self):
+    def close(self) -> None:
         self._is_window_running = False
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from Window import Window
+from window import Window, Point, Line
 
 
-def main():
+def main() -> None:
     screen = Window(800, 800)
+
+    point1 = Point(0, 0)
+    point2 = Point(400, 400)
+    line = Line(point1, point2)
+    screen.draw_line(line, "black")
+    point1 = point2
+    point2 = Point(800, 0)
+    line = Line(point1, point2)
+    screen.draw_line(line, "red")
+
     screen.wait_for_close()
 
 
