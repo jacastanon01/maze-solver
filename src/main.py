044cc53486ad7ebbd1378708d414b5f365379eb8
Jacob Castanon <jacastanon01@gmail.com>
2024-04-17 12:11:40 -0500

    tested Cell draw method by rendering grid

--- src/main.py
+++ src/main.py
@@ -2,28 +2,20 @@ from screen import Screen, Line, Point, Cell
 
 
 def main() -> None:
-    screen = Screen(800, 800)
+    window = Screen(800, 800)
+    render_grid(window)
+    window.wait_for_close()
 
-    for i in range(0, 800, 200):
-        start = i
-        end = i + 200
-        cell = Cell(screen, start, 0, end, 200)
-        cell.draw()
 
-    # point1 = Point(0, 0)
-    # point2 = Point(400, 400)
-    # line = Line(point1, point2)
-    # screen.draw_line(line, "black")
-    # point1 = point2
-    # point2 = Point(800, 0)
-    # line = Line(point1, point2)
-    # screen.draw_line(line, "red")
-    # point1 = point2
-    # point2 = Point(300, 600)
-    # line = Line(point1, point2)
-    # screen.draw_line(line, "blue")
-
-    screen.wait_for_close()
+def render_grid(screen: Screen) -> None:
+    for row in range(0, 800, 200):
+        for col in range(0, 800, 200):
+            start_x1 = col
+            start_y1 = row
+            end_x2 = col + 200
+            end_y2 = row + 200
+            cell = Cell(screen, start_x1, start_y1, end_x2, end_y2)
+            cell.draw()
 
 
 if __name__ == "__main__":
