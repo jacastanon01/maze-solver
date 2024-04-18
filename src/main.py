f180b1e02cd204a721c68473bc05e85fd75a526f
Jacob Castanon <jacastanon01@gmail.com>
2024-04-17 22:18:11 -0500

    tried to find a pattern in maze construction. Will need to use specific maze algorithm

--- src/main.py
+++ src/main.py
@@ -1,34 +1,54 @@
+from random import randint, choice
+
 from screen import Screen, Line, Point, Cell
 
 
 def main() -> None:
     window = Screen(800, 800)
+    render_maze(window)
     # render_grid(window)
-    c1 = Cell(window)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
+    # c1 = Cell(window)
+    # c1.has_right_wall = False
+    # c1.draw(50, 50, 100, 100)
 
-    c2 = Cell(window)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
+    # c2 = Cell(window)
+    # c2.has_left_wall = False
+    # c2.has_bottom_wall = False
+    # c2.draw(100, 50, 150, 100)
 
-    c1.draw_move(c2)
+    # c1.draw_move(c2)
 
-    c3 = Cell(window)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
+    # c3 = Cell(window)
+    # c3.has_top_wall = False
+    # c3.has_right_wall = False
+    # c3.draw(100, 100, 150, 150)
 
-    c2.draw_move(c3)
+    # c2.draw_move(c3)
 
-    c4 = Cell(window)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
+    # c4 = Cell(window)
+    # c4.has_left_wall = False
+    # c4.draw(150, 100, 200, 150)
 
-    c3.draw_move(c4, True)
+    # c3.draw_move(c4, True)
 
     window.wait_for_close()
+
+
+def render_maze(screen: Screen) -> None:
+    n = randint(3, 8)
+    x, y = 0, 0
+    for i in range(n):
+        c = Cell(screen)
+        x2 = x + 50
+        y2 = y + 50
+
+        if i >> 1 == 0:
+            x += 50
+            x2 += 50
+        else:
+            y += 50
+            y2 += 50
+        c.draw(x, y, x2, y2)
 
 
 def render_grid(screen: Screen) -> None:
