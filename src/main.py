b4cd41a4521a1d02fd574345c823693499b90469
Jacob Castanon <jacastanon01@gmail.com>
2024-04-25 01:06:56 -0500

    reorganizing main

--- src/main.py
+++ src/main.py
@@ -5,8 +5,17 @@ from src.screen import Window
 
 
 def main() -> None:
-    window = Window(800, 800)
-    my_maze = Maze(10, 10, 6, 4, 10, 10)
+    print("Welcome to the maze solver!", end="\n==================\n\n")
+    print("Please enter the dimensions of your maze:")
+    cols = int(input("Columns: "))
+    rows = int(input("Rows: "))
+    width = cols * 50
+    height = rows * 50
+    print(f"\nYour maze is {cols} columns by {rows} rows.")
+    window = Window(width, height)
+    padding_x = width // 2
+    padding_y = height // 2
+    my_maze = Maze(padding_x, padding_y, cols, rows, 10, 10)
     maze = MazeDrawer(my_maze, window)
     window.start()
     window.wait_for_close()
--- src/maze.py
+++ src/maze.py
@@ -23,8 +23,8 @@ class Cell:
 
     Methods
     -----
-    draw(x1 : int, y1 : int, x2 : int, y2 : int) -> None : Draws cell to screen
-    draw_move(to_cell : Cell, undo ?: bool) -> None : Draws line thru cells
+    draw(x1 : int, y1 : int, x2 : int, y2 : int)  : Draws cell to screen
+    draw_move(to_cell : Cell, undo ?: bool)  : Draws line thru cells
     get_wall_directions -> dict[{str: tuple(int)}] : Retrieves the coordinates for a given wall direction
     """
 
@@ -155,7 +155,7 @@ class Maze:
     Attributes
     -----
     x_start : int : Represents how many pixels from the left the maze runner should start
-    x_start : int : Represents how many pixels from the top the maze runner should start
+    y_start : int : Represents how many pixels from the top the maze runner should start
     num_cols : int : Total cell columns
     num_rows : int : Total cell rows
     cell_width : int : Cell width
@@ -165,7 +165,7 @@ class Maze:
 
     Methods
     -----
-    init_cells -> None : Initializes the matrix with Cell objects
+    init_cells  : Initializes the matrix with Cell objects
     get_neighbor_coords(col: int, row: int, direction: str) -> Tuple[Tuple[int, int], str]
         Returns the coordinates of the neighbor cell and the direction of the wall that connects them  
     get_cell(col: int, row: int) -> Cell : Returns the cell at the specified column and row
@@ -303,12 +303,12 @@ class MazeDrawer:
 
     Methods:
     ----
-    create_cells -> None : Initializes the matrix of cells and draws them to screen
-    draw_cell(i: int, j: int) -> None : Draws a cell to screen at specified row/column position
-    animate -> None : Animates maze by drawing cells one at a time and allows us to visulize our algorithm
-    draw_entrance_and_exit -> None : Draws entrance and exit to maze by removing the top wall of the first cell and
+    - create_cells  : Initializes the matrix of cells and draws them to screen
+    - draw_cell(i: int, j: int) : Draws a cell to screen at specified row/column position
+    - animate : Animates maze by drawing cells one at a time and allows us to visulize our algorithm
+    - draw_entrance_and_exit  : Draws entrance and exit to maze by removing the top wall of the first cell and
     the bottom wall of the last cell
-    break_walls_r(col : int, row : int) -> None : Recursive backtracking algorithm to create maze   
+    - break_walls_r(col : int, row : int)  : Recursive backtracking algorithm to create maze   
     """
 
     def __init__(self, maze: Maze, window: Window):
@@ -320,7 +320,6 @@ class MazeDrawer:
         self._create_entrance_and_exit()
         self._break_walls_r(0, 0)
         self._maze.reset_visited_cells()
-
 
     def _create_cells(self) -> None:
         """Draws matrix of cells to draw to screen"""
@@ -347,7 +346,7 @@ class MazeDrawer:
     def _animate(self) -> None:
         """Animates maze by drawing cells one at a time and allows us to visulize our algorithm"""
         self._window.redraw()
-        time.sleep(0.1)
+        time.sleep(0.05)
 
     def _create_entrance_and_exit(self) -> None:
         """Creates entrance and exit to maze by removing the top wall of the first cell and
